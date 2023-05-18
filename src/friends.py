2d606d0e35b9df03c0d39c307f6bd02d1ece0308
Ewan Laing <ewanlaing@Ewans-MacBook-Air.local>
2023-05-18 14:24:36 +0100

    9 questions answered

--- src/friends.py
+++ src/friends.py
@@ -26,3 +26,16 @@ def lend_money(person_1, person_2, amount):
     person_1["monies"] -= amount
     person_2["monies"] += amount
 
+def all_favourite_foods(people):
+    foods = []
+    for person in people:
+        foods += person["favourites"]["snacks"]
+    return foods
+
+def find_no_friends(people):
+    no_friends = []
+    for person in people:
+        if len(person["friends"]) == 0:
+            no_friends.append(person)
+    return no_friends
+
