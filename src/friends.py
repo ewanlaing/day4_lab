55b784cf0cd5609eb8e35acae4eb5d1ee3f3eb22
Ewan Laing <ewanlaing@Ewans-MacBook-Air.local>
2023-05-18 13:59:29 +0100

    4 questions done

---
+++ src/friends.py
@@ -0,0 +1,14 @@
+def get_name(person):
+    return person["name"]
+
+def get_favourite_tv_show(person):
+    return person["favourites"]["tv_show"]
+
+def likes_to_eat(person, food):
+    for snack in person["favourites"]["snacks"]:
+        if snack == food:
+            return True
+    return False
+
+def add_friend(person, new_friend):
+    person["friends"].append(new_friend)
